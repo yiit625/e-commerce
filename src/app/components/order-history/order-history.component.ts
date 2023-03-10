73558a13034d2e6a38ad7870ac75dbe343f4e675
Pvsagar18 <127306449+Pvsagar18@users.noreply.github.com>
2023-03-10 15:51:09 +0530

    Update order-history.component.ts

--- src/app/components/order-history/order-history.component.ts
+++ src/app/components/order-history/order-history.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { CartapiService } from 'src/app/services/cartapi.service';
 
 @Component({
   selector: 'app-order-history',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-history.component.css']
 })
 export class OrderHistoryComponent {
+  displayedColumns: string[] | undefined;
+  pageIndex: number = 0;
+  pageSize: number = 10;
+  // dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
+  previousProducts: any = 0;
 
+  constructor(private cartApi: CartapiService) {
+  }
+  ngOnInit() {
+    this.displayedColumns = ['imageUrl','title'];
+    this.previousProducts = this.cartApi.purchasedList;
+    console.log(this.previousProducts);
+  }
+  onChangePage(pe: PageEvent) {
+    this.pageSize = pe.pageSize;
+    this.pageIndex = pe.pageIndex;
+  }
 }
